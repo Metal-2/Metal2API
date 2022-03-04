1ae68a50f328fc24680e0452ec530a2200e1729e
tecnofany <tecnofany@gmail.com>
2022-03-03 20:01:55 -0500

    git

--- headers.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: * ');
-header('Content-Type: aplication/json');
-header('Access-Control-All-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
-header('Acces-Control-Allow-Headers:Access-Control-Allow-Headers,Content-Type,Access-Control-All-Methods,Authorization, X-Requested-With');
